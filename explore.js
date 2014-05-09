acb6f365950a2e19b971ad2f9c552523add70615
Mike Schaeffer <mschaeffer@ksmpartners.com>
2014-05-08 21:14:35 -0400

    Display graphic via JS.

--- explore.js
+++ explore.js
@@ -6,6 +6,14 @@ function onDocumentReady()
     var context = example.getContext('2d');
     context.fillStyle = 'red';
     context.fillRect(30, 30, 50, 50);
+
+    var tiles = new Image();
+
+    tiles.src="pics/dg_grounds32.gif";
+
+    tiles.onload = function() {
+        context.drawImage(tiles, 69, 50);
+    };
 };
 
 $(document).ready(onDocumentReady);
