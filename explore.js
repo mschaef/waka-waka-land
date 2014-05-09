a8cd9dda0758ffa9a5cd577223cccc81ad8dbb46
Mike Schaeffer <mschaeffer@ksmpartners.com>
2014-05-08 21:08:41 -0400

    Add a canvas and code to draw into it.

--- explore.js
+++ explore.js
@@ -1,3 +1,12 @@
-$(document).ready(function ()
-                  {
-                  });
+
+
+function onDocumentReady()
+{
+    var example = document.getElementById('map');
+    var context = example.getContext('2d');
+    context.fillStyle = 'red';
+    context.fillRect(30, 30, 50, 50);
+};
+
+$(document).ready(onDocumentReady);
+
