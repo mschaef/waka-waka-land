16f74b0d65c697e8a3417b273d32c86bb8c88815
Mike Schaeffer <mike@mschaef.com>
2020-10-10 11:04:05 -0400

    Update gif2js to work with giflib >5.1.0

--- gif2js/gif2js.c
+++ gif2js/gif2js.c
@@ -6,6 +6,9 @@
 
 #include <stdio.h>
 
+// This requires the venerable giflib to build. On OSX,
+// 'brew install giflib' will work to install it if you're
+// using homebrew.
 #include <gif_lib.h>
 
 void render_js(GifFileType *gif)
@@ -48,10 +51,12 @@ int main(int argc, char* argv[])
      GifFileType *gif;
      int rc;
 
-     gif = DGifOpenFileName(argv[1]);
+
+     gif = DGifOpenFileName(argv[1], &rc); 
 
      if (gif == NULL) {
-          fprintf(stderr, "Usage: Cannot open file: %s\n", argv[1]);
+          fprintf(stderr, "Usage: Cannot open file: %s (errorcode: %d)\n",
+                  argv[1], rc);
           return 1;
      }
 
@@ -63,10 +68,9 @@ int main(int argc, char* argv[])
           fprintf(stderr, "Usage: Cannot read file: %s\n", argv[1]);
      }
      
-     rc = DGifCloseFile(gif);
-
-     if (rc != GIF_OK) {
-          fprintf(stderr, "Usage: Error closing file: %s\n", argv[1]);
+     if (DGifCloseFile(gif, &rc) != GIF_OK) {
+          fprintf(stderr, "Usage: Error closing file: %s (errorcode: %d(\n",
+                  argv[1], rc);
           return 1;
      }
 
